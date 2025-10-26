7ee20e960072b59f71ba424354f3cf0ab1ae3d3e
baohexu <3143064814@qq.com>
2025-10-26 23:17:21 +0800

    Update ku最后.py

--- ku最后.py
+++ ku最后.py
@@ -5,8 +5,8 @@ from torch_geometric.datasets import Planetoid
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-dataset = Planetoid(root='D:\\\\QQ\\\\cora', name='Cora')
-data = dataset[0].to(device)
+dataset = Planetoid(root='D:\\QQ\\cora', name='Cora')
+data = dataset[0]
 
 class CoraDataset(Dataset):
     def __init__(self, pyg_data, mode='train'):
@@ -14,25 +14,24 @@ class CoraDataset(Dataset):
         self.mode = mode
         
         self.features_df = pd.DataFrame(
-            self.data.x.cpu().numpy(),
-            columns=[f'feat_{i}' for i in range(self.data.x.shape[1])]
+           pyg_data.x.cpu().numpy(),
+            columns=[f'feat_{i}' for i in range(pyg_data.x.shape[1])]
         )
         
         self.labels_series = pd.Series(
-            self.data.y.cpu().numpy(),
+             pyg_data.y.cpu().numpy(),
             name='label'
         )
         
         if mode == 'train':
-            mask = self.data.train_mask.cpu().numpy()
+           mask = pyg_data.train_mask.cpu().numpy()
         elif mode == 'val':
-            mask = self.data.val_mask.cpu().numpy()
+            mask = pyg_data.val_mask.cpu().numpy()
         else:
-            mask = self.data.test_mask.cpu().numpy()
+            mask = pyg_data.test_mask.cpu().numpy()
         
-        self.mask_series = pd.Series(mask, name='mask')
+        
         self.indices = pd.Series(range(len(mask)))[mask].values
-        
         self.full_df = pd.concat([self.features_df, self.labels_series], axis=1)
         self.subset_df = self.full_df.loc[self.indices].reset_index(drop=True)
         
@@ -56,4 +55,5 @@ print(f"特征维度: {train_dataset.features.shape[1]}")
 print(f"类别数量: {len(torch.unique(train_dataset.labels))}")
 
 features, label = train_dataset[0]
+
 print(f"样本特征形状: {features.shape}, 标签: {label.item()}")
